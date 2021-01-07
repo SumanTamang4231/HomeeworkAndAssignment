fa7e97f2d101f5bba0ee851fffb321f14c25d2f5
Suman Tamang <subanlama4231@gmail.com>
2021-01-06 18:32:50 -0600

    Added array assignment

---
+++ src/array/Assignment31.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Assignment31 {
+    public static void main(String[] args) {
+        int []array = new int[5];
+        array[0]=1;
+        array[1]=12;
+        array[2]=123;
+        array[3]=1234;
+        array[4]=12345;
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}
