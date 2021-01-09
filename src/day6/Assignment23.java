31632e1966a6dcc4dbcc552db99793987575981b
Suman Tamang <subanlama4231@gmail.com>
2021-01-08 19:51:25 -0600

    Added Assignment23

---
+++ src/day6/Assignment23.java
@@ -0,0 +1,19 @@
+package day6;
+
+import java.util.Scanner;
+
+public class Assignment23 {
+    public static void main(String[] args) {
+  int num,count ,total=0;
+        System.out.println("Please enter number of the natural number ");
+        Scanner input=new Scanner(System.in);
+        num =input.nextInt();
+
+        for(count = 1; count <= num; count++){
+            total = total + count;
+        }
+
+        System.out.println("Sum of first "+num+" natural numbers is: "+total);
+
+    }
+}
